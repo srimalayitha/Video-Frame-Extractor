4e96a42e4e60fe7592403f2ed5088e72ebc456f5
srikanth mollety <srikanth.mollety@outlook.com>
2024-09-02 14:39:41 +0100

    Testing

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    plugins: ['react'],
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      // Add any custom rules or override default rules here
+    },
+  };
+  
